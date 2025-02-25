e47a7ec086b3e28a7de533ddf76549586a81f9dc
spk <102252652+s3cr3t-on-twitch@users.noreply.github.com>
2025-02-25 13:02:00 -0500

    mainMenu.java updated with custom window

--- mainMenu.java
+++ mainMenu.java
@@ -1,3 +1,28 @@
-//menu here
-//also graphics main class
-//also money track
+//NOT SWOPE-GIVEN STARTER CODE, going off side scroller
+//------
+// Class: GraphicsMain
+// Written by: Mr. Swope
+// Date: 1/27/2020
+// Description: This class contains the main method for this project. You shouldn't modify this class.
+//              This class must be selected when you run your project.
+//Edited
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class GraphicsMain extends JFrame{
+
+	public static void main(String[] args) {
+		GraphicsMain window = new GraphicsMain();
+	    JPanel p = new JPanel();
+	    p.add(new GraphicsPanel());  
+	    window.setTitle("Pennsylvania Casino");
+	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    window.setContentPane(p);
+	    window.pack();
+	    window.setLocationRelativeTo(null);
+	    window.setVisible(true);
+
+	}
+
+}
