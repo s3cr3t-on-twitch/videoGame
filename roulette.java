335187eb65ce5bc0e47845ece5edd88c255d86ba
spk <102252652+s3cr3t-on-twitch@users.noreply.github.com>
2025-02-26 09:03:35 -0500

    Update roulette.java

--- roulette.java
+++ roulette.java
@@ -1,1 +1,6 @@
 //roulette wheel - https://www.primeapi.com/cmscdn/cdn/cms/GZ/American_Roulette_Wheel_c77cdea366.webp
+public static void roulette{
+	ImageIcon wheel;
+  String imagePath = "images/kenny.png";
+
+}
