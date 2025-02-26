e751b7dac934304b4c9e7178b0e18994d1fd3956
spk <102252652+s3cr3t-on-twitch@users.noreply.github.com>
2025-02-26 09:21:40 -0500

    Update roulette.java

--- roulette.java
+++ roulette.java
@@ -1,6 +1,11 @@
 //roulette wheel - https://www.primeapi.com/cmscdn/cdn/cms/GZ/American_Roulette_Wheel_c77cdea366.webp
 public static void roulette{
+	ClassLoader cldr = this.getClass().getClassLoader();
 	ImageIcon wheel;
-  String imagePath = "images/kenny.png";
+  String imagePath = "images/rouletteWheel.webp"; //TODO - upon importing to eclipse, add rouletteWheel to a new "images" folder, also, may not take .webp images, maybe change to .png?
+URL imageURL = cldr.getResource(imagePath);
+wheel = new ImageIcon(imageURL);
+g2.drawImage
+
 
 }
