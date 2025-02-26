f0b8e4e3335d7d8a222a2a26103d6069c474ba79
spk <102252652+s3cr3t-on-twitch@users.noreply.github.com>
2025-02-26 08:54:22 -0500

    Update GraphicsMain.java

--- GraphicsMain.java
+++ GraphicsMain.java
@@ -10,7 +10,7 @@ public class GraphicsMain extends JFrame{
 		GraphicsMain window = new GraphicsMain();
 	    JPanel p = new JPanel();
 	    p.add(new GraphicsPanel()); 
-	    window.setTitle("Chess");
+	    window.setTitle("Pennyslvania Casino");
 	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    window.setContentPane(p);
 	    window.pack();
